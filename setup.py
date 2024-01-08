93ff493aeeaf006fc1c6b00c9c7a2c801b0cc984
mrichard91 <mjr@stairwell.com>
2024-01-08 22:08:46 +0000

    Fixup merge issue

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # http://pypi.python.org/pypi/setuptools
 NAME = "stairwell-api"
 VERSION = "1.0.0"
-PYTHON_REQUIRES = ">=3.7"
+PYTHON_REQUIRES = ">=3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
     "python-dateutil",
